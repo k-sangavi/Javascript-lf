4385499a19dcf90d77bdd76905f4fc15b6274d4b
Sangavi Karthi <sangavidev24@gmail.com>
2024-12-26 23:03:55 +0530

    JS intro

---
+++ script.js
@@ -0,0 +1,16 @@
+let amount = 0  // initialization
+// assignment
+amount = 1000
+// score = ' no score'
+
+const pi = 3.14
+var val = 10
+
+console.log("Your bill amount is Rs." + amount);
+
+        //  console.log('hello') to write this message in chrome console page (ctrl+shift+J)
+        // alert("hiii") to write this meassage in alert type of message showed. 
+        // alert and console type of code is A line of code (statement)
+//  multi line comments 
+// variables - variables are stored in data ( ex: let,..) //
+ 

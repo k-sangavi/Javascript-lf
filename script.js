a2f9053bed15f57d9094c5c766b84c4820d397f7
Sangavi Karthi <sangavidev24@gmail.com>
2025-01-02 23:30:38 +0530

    updated

--- script.js
+++ script.js
@@ -13,6 +13,15 @@ document.write("<h1>hello world</h1>");
 var a=prompt ("ENTER A STRING INFORMATION");
 console.log(a);
 
+
+var a1;
+var a2=8;
+var a3="This is my webpage</br>";
+var a4="Javascript tutorials</br>";
+document.write(a3+a4);
+
+
+
         //  console.log('hello') to write this message in chrome console page (ctrl+shift+J)
         // alert("hiii") to write this meassage in alert type of message showed. 
         // alert and console type of code is A line of code (statement)

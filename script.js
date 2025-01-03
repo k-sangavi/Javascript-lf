c2f3b71bc070408fbfebedbfdd3b76f807b15035
Sangavi Karthi <sangavidev24@gmail.com>
2025-01-03 22:52:02 +0530

    updated

--- script.js
+++ script.js
@@ -8,10 +8,17 @@ var val = 10
 
 console.log("Your bill amount is Rs." + amount);
 console.log("Hello world");
-alert("hello");
+// alert("hello");
 document.write("<h1>hello world</h1>");
-var a=prompt ("ENTER A STRING INFORMATION");
+var a=("ENTER A STRING INFORMATION");
+// var a=prompt ("ENTER A STRING INFORMATION");
 console.log(a);
+   //  console.log('hello') to write this message in chrome console page (ctrl+shift+J)
+        // alert("hiii") to write this meassage in alert type of message showed. 
+        // alert and console type of code is A line of code (statement)
+//  multi line comments 
+// variables - variables are stored in data ( ex: let,..) //
+ 
 
 
 var a1;
@@ -33,9 +40,52 @@ var c5=null;
 console.log(typeof c5);
 
 
-        //  console.log('hello') to write this message in chrome console page (ctrl+shift+J)
-        // alert("hiii") to write this meassage in alert type of message showed. 
-        // alert and console type of code is A line of code (statement)
-//  multi line comments 
-// variables - variables are stored in data ( ex: let,..) //
- 
+var n1=23;
+console.log(typeof n1);
+var n2="hello";
+console.log(typeof n2);
+var n3=(n1+n2);
+console.log(typeof n3);
+var n4=("enter a value");
+// var n4=prompt("enter a value");
+console.log(typeof n4);
+//before is implicit 
+// after code is explicit
+var n5=Number(15);
+console.log(typeof n5);
+var n6=Number("hello");
+console.log(typeof n6);
+var n7=String(18);
+console.log(typeof n7);
+var n8=Boolean("hii");
+console.log(typeof n8);
+var n9="youtube";
+console.log(typeof parseInt(n9));
+var n10="youtube";
+console.log(typeof parseFloat(n10));
+
+// functions- 4 types
+// 1.Named function
+// 2.Anonymous function
+// 3.Constructor function
+// 4.Self invoking function
+
+// 1.Named function
+function add(a,b)
+{return a+b;}
+console.log (add(3,5));
+
+// 2. Anonymous function
+var a=function(a,b){
+return a+b;
+}
+console.log (a(5,5));
+setTimeout(function(){console.log("this is anonymous function")}1000);
+// Anonymous function does not have function name.
+
+
+
+
+
+
+       
